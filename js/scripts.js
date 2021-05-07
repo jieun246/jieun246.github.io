67c06841211a49d7f2694a58fc93a1ba5ccef1fa
jieun246 <idgocm@naver.com>
2021-05-08 02:16:08 +0900

    포트폴리오 수정중

--- js/scripts.js
+++ js/scripts.js
@@ -54,7 +54,7 @@
     $(window).scroll(navbarCollapse);
 
     // Magnific popup calls
-    $('#portfolio').magnificPopup({
+    /*$('#portfolio').magnificPopup({
         delegate: 'a',
         type: 'image',
         tLoading: 'Loading image #%curr%...',
@@ -67,6 +67,6 @@
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
-    });
+    });*/
 
 })(jQuery); // End of use strict
